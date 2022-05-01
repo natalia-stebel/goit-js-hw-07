6512d07b7a713fb0e862ee866916a93ac1d72681
natalia-stebel <93938115+natalia-stebel@users.noreply.github.com>
2022-05-01 19:37:12 +0300

    task 02 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const createGalleryItem = ({ preview, original, description }) => {
+  return `
+ <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+    style="display:block"
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    data-lightbox="images"
+    />
+  </a>
+</div>
+    `;
+};
+
+const createGalleryContainer = galleryItems.map(createGalleryItem).join('');
+const galleryImages = document.querySelector('.gallery');
+
+galleryImages.insertAdjacentHTML('beforeend', createGalleryContainer);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
